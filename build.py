a19d19716c1cdfcc8eee83689c41deacf3713740
Pedro Figueiras <pfigueiras@outlook.com>
2023-09-06 00:17:28 +0200

    Update build.py to run tests

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import subprocess
 
 def build_project():
-	subprocess.run(["python", "src/mycode.py"])
+    subprocess.run(["python", "src/mycode.py"])
+    subprocess.run(["python", "-m", "unittest", "src/test1.py"])
+    subprocess.run(["python", "-m", "unittest", "src/test2.py"])
 
 if __name__ == "__main__":
-	build_project()
+    build_project()
