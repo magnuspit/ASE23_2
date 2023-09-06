bb0548c600e6d57823493f7c43be067187f32806
Pedro Figueiras <pfigueiras@outlook.com>
2023-09-07 00:31:34 +0200

    New method test

--- build.py
+++ build.py
@@ -2,8 +2,6 @@ import subprocess
 
 def build_project():
     subprocess.run(["python", "src/mycode.py"])
-    subprocess.run(["python", "-m", "unittest", "src/test1.py"])
-    subprocess.run(["python", "-m", "unittest", "src/test2.py"])
 
 if __name__ == "__main__":
     build_project()
---
+++ src/test.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+def test_output():
+    result = subprocess.run(["python", "build.py"], capture_output=True, text=True)
+    output = result.stdout.strip()
+
+    if output == "6":
+        print("Test passed: Output is 6")
+    else:
+        print("Test failed: Unexpected output")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    test_output()
+
+
